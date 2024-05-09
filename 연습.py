6bc86c03bf4043d8aa9e8502b4ea59e5d91b414f
cutepuppy2024 <danakim0209@gmail.com>
2024-05-09 18:41:35 +0900

    jupyter

--- 연습.py
+++ 연습.py
@@ -1,42 +1,75 @@
-import requests
-import time
-import random
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36","Accept-Language":"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"}
-url = "https://watcha.com/browse/webtoon"
-#브라우저 열기
-browser = webdriver.Chrome()
-browser.maximize_window() # 창 최대화
-browser.get(url)
-## 화면 스크롤 내리기
-# 현재 스크롤 높이 검색
-prev_height = browser.execute_script("return document.body.scrollHeight")
-print("최초 높이 : ",prev_height)
-# 스크롤 이동
-while True:
-    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    time.sleep(2)
-    # 재조정된 스크롤 높이
-    curr_height = browser.execute_script("return document.body.scrollHeight")
-    if prev_height == curr_height:
-        break # 같으면 중단하고 빠져나옴.
-    prev_height = curr_height
-    print("현재 높이 : ",curr_height)
-# 데이터 찾기
-soup = BeautifulSoup(browser.page_source,"lxml")
-sections = soup.find_all("section",{"class":"custom-11ytuur e1134x5i3"})
-print("개수 : ",len(sections))
-uls = sections[14].find("ul",{"class":"evjjsye0 custom-plh92q-Row e8ldpmn0"})
-lis = uls.find_all("li",{"class":"w_exposed_cell e1bnpttb3 custom-vnoe9g-RowItem etpnybg1"})
-print("개수 : ",len(lis))
-alts = lis[0].find("img")
-print(lis[0].find("img"))
-print("제목 : ",alts["alt"])
-input("Enter키 입력시 프로그램이 종료됩니다. >>")
-# 화면 닫기
-browser.quit()
+import pandas as pd
+df = pd.read_excel('score.xlsx',index_col='지원번호')
+df
+
+# df = pd.read_excel('20122022_출생.xlsx',skiprows=2,nrows=3, usecols='B:M',index_col=0)
+# df.index.name = '제목'
+# df.index
+
+# 문자열 함수
+# slice : 문자열 자르기
+# df_str['idx'].str.slice(1,3)
+# # df_str['idx'].map(lambda x:x[1:3])  #map(함수) , lambda : 익명함수
+
+# # split : 문자열 분리
+# a_list = ['데이터,분석가','영희,철수,바둑이','국어,영어,수학,과학,사회']
+# data = {"d_split":a_list}
+# df_str = pd.DataFrame(data)
+# s_data = df_str['d_split'].str.split(',') #배열로 분리되어 리턴
+# s_data
+
+# replace:문자열 처리, strip:공백제거
+
+
+# 조건 &, |
+filt  = df['키']>188  #조건식 loc
+filt
+df.loc[filt]
+
+# 2개 row데이터 출력
+df.loc[['1번','5번']]
+# 1번학생의 키 값만 출력
+df.loc['1번','키']
+df.loc['1번',['이름','키']] # 1번 학생의 이름,키 출력
+df.loc[['1번','5번'],['이름','키']]
+df.loc['1번':'5번','국어':'사회']
+
+# rows데이터 가져오기
+df[0:3]
+df[5:]
+# df[[0,1,3]] #error
+df.iloc[[0,1,3]]   # rows데이터 부분적으로 가져오기
+df.head()
+df.tail(2)
+
+# 컬럼별 호출
+df['키']
+df['이름']
+df[['키','이름']][1:4]
+df[df.columns[[1,3,4]]][1:4]
+# 컬럼만 가져오기
+df.columns
+df.columns[0:3]
+df.columns[[1,4,6]]
+
+df['키'].max()
+df['키'].min()
+df['키'].mean()
+df['키'].count()
+df['국어'].sum()
+df['키'].describe()
+df['키'].info()
+df['grade'].unique() # 중복제거
+
+df['SW특기'].count() # Nan데이터는 개수에 들어가지 않음.
+df['키'].nlargest(4) # 키 큰순으로 3개 가져옴.
+df['키'].nsmallest(3) # 키 작은순으로 3개 가져옴
+
+df.describe() # 컬럼별 대략적인 정보, 최소값,최대값,평균 등 확인
+df.head()     # 상단 5개 출력
+df.tail()     # 하단 5개 출력
+df.info()     # 컬럼별 타입,크기 정보
+df.values     # rows데이터 배열로 출력
+df.index      # index정보
+df.columns    # 컬럼 정보
+df.shape      # 데이터 크기 
